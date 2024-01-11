89e0db3d3b60a42c3507ea44cb957896ac9b01d4
noypidev <remus.victuelles@gmail.com>
2024-01-11 20:23:40 +1100

    Added preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-   integrations: [mdx()],
-   site: "https://test.com"
+  integrations: [mdx(), preact()],
+  site: "https://test.com"
 });
